5332386eac7f8bd46b3fec875e09d2d8ec29b31b
michal <namoopsoo>
2023-03-19 22:00:06 -0400

    moar

--- 2023-03-18-notebook.py
+++ 2023-03-18-notebook.py
@@ -17,51 +17,46 @@ def make_xtick_labels(x, step=5):
     return x_ticks, x_labels
 
 root_dir = Path(os.getenv("ROOT_DIR"))
+
+# Prepare food df
 food_logs_loc = (root_dir / "CarbManager/daily_logs_20210312-20230318_fe628b90-9859-4259-ab49-1ba3ce95b90c.csv")
-weight_loc = root_dir / ("withings/2023-03-18-data_MIC_1679186118/weight.csv")
 food_df = pd.read_csv(food_logs_loc)
-weight_df = pd.read_csv(weight_loc)
 
 cols = ["RawDate", "Meal", "Calories"]
 food_df["RawDate"] = food_df["Date"].map(lambda x: x.split(" ")[0])
 food_df[food_df.Meal.isin(["Snack", "Dinner", "Lunch", "Breakfast"])][cols].iloc[-10:]
-
-In [30]: food_df[["RawDate", "Calories"]].groupby(by=["RawDate"]).sum().Calories.describe()
-Out[30]: 
-count     452.000000
-mean     2673.786062
-std       778.165650
-min         0.000000
-25%      2296.875000
-50%      2685.450000
-75%      3098.575000
-max      5062.000000
-Name: Calories, dtype: float64
-    
-    
 food_agg_df = food_df[["RawDate", "Calories"]].groupby(by=["RawDate"]).sum().reset_index()
 
 
-cols = ["RawDate", "Weight (lb)"]
+weight_loc = root_dir / ("withings/2023-03-18-data_MIC_1679186118/weight.csv")
+weight_df = pd.read_csv(weight_loc)
+cols = ["Date", "Weight (lb)"]
 weight_df[cols].sort_values(by="Date").iloc[-10:]
 weight_df["RawDate"] = weight_df["Date"].map(lambda x: x.split(" ")[0])
+cols = ["RawDate", "Weight (lb)"]
+weight_agg_df = weight_df[cols].groupby(by="RawDate").min().reset_index()
 
-start_date = date(2023, 1, 1) # "2023-01-01"
+start_date = date(2022, 1, 1) # "2023-01-01"
 end_date = date(2023, 3, 17)
-x = [(start_date + timedelta(days=i)).strftime("%Y-%m-%d") 
+X = [(start_date + timedelta(days=i)).strftime("%Y-%m-%d") 
      for i in range((end_date - start_date).days)]
 fig = plt.figure(figsize=(12, 4))
 ax = fig.add_subplot(111)
 
-data =  food_agg_df[food_agg_df["RawDate"] >= start_date.strftime("%Y-%m-%d")
+data1 =  food_agg_df[food_agg_df["RawDate"] >= start_date.strftime("%Y-%m-%d")
                      ].to_dict(orient="list")
-x1, y1 = data["RawDate"], data["Calories"]
+x1, y1 = data1["RawDate"], data1["Calories"]
 ax.plot(x1, y1)
-x_ticks, x_labels = make_xtick_labels(x, step=7)
+x_ticks, x_labels = make_xtick_labels(X, step=7)
 ax.set_xticks(x_ticks)
 ax.set_xticklabels(x_labels, rotation=-45)
-# fig.show()
+
+# weight data, 
+data2 = weight_agg_df[weight_agg_df["RawDate"] >= start_date.strftime("%Y-%m-%d")
+                     ].to_dict(orient="list")
+x2, y2 = data2["RawDate"], data2["Weight (lb)"]
+ax.plot(x2, y2)
 
 
-out_loc = "/Users/michal/Dropbox/Code/repo/fasting-analyze/output-data/blah.png"
+out_loc = f"output-data/{utc_ts()}-figure.png"
 pylab.savefig(out_loc, bbox_inches='tight')
